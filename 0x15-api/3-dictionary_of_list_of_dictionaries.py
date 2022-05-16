73bc825ab9f48ae0b459a41ba17e51758befc3ac
Anthony-6 <anthony.lacoste5@gmail.com>
2022-05-16 17:41:39 +0200

    update file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
 
     user = requests.get("https://jsonplaceholder.typicode.com/users")
     todolist = requests.get("https://jsonplaceholder.typicode.com/todos")
-    tasksL = []
     tasksD = {}
 
     for u in user.json():
+        tasksL = []
         for t in todolist.json():
             if t.get("userId") == u.get("id"):
-                tasktodict = {"username": u.json().get("username"),
+                tasktodict = {"username": u.get("username"),
                               "task": t.get("title"),
                               "completed": t.get("completed")}
                 tasksL.append(tasktodict)
