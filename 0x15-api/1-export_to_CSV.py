89df878cd9ed71e97f504eda741536913ce6bb05
Anthony-6 <anthony.lacoste5@gmail.com>
2022-05-16 13:11:42 +0200

    add file for task n°1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' Python script that export employee data information in csv file '''
+if __name__ == "__main__":
+
+    import requests
+    import sys
+    import csv
+
+    userId = sys.argv[1]
+    user = requests.get(f'https://jsonplaceholder.typicode.com/users/{userId}')
+    todolist = requests.get(f'https://jsonplaceholder.typicode.com/todos')
+    name = user.json().get('name')
+    filename = userId + '.csv'
+
+    with open(filename, mode="w", newline='') as myfile:
+        writer = csv.writer(myfile, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for t in todolist.json():
+            if t.get('userId') == int(userId):
+                writer.writerow([userId, name, t.get('completed'),
+                                 t.get('title')])
