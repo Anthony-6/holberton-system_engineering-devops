47025bf9c7d132d74a4cac33d0a2bd7c2e47470a
Anthony-6 <anthony.lacoste5@gmail.com>
2022-05-16 12:53:25 +0200

    add file

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' Python script that return information about employee TODO list '''
+if __name__ == "__main__":
+
+    import requests
+    import sys
+
+    userId = sys.argv[1]
+    user = requests.get(f'https://jsonplaceholder.typicode.com/users/{userId}')
+    todolist = requests.get(f'https://jsonplaceholder.typicode.com/todos')
+
+    name = user.json().get('name')
+    doneTask = 0
+    totalTask = 0
+
+    for t in todolist.json():
+        if t.get('userId') == int(userId):
+            totalTask += 1
+            if t.get('completed'):
+                doneTask += 1
+
+    print(f'Employee {name} is done with tasks({doneTask}/{totalTask}):')
+    for t in todolist.json():
+        if t.get('userId') == int(userId) and t.get('completed'):
+            print("\t {}".format(t.get('title')))
