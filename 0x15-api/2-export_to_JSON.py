e6d4f9bc7d7d6efcf7ec23398bc2b0208f8aad5c
Anthony-6 <anthony.lacoste5@gmail.com>
2022-05-16 17:26:12 +0200

    update file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,19 +7,20 @@ if __name__ == "__main__":
     import json
 
     userId = sys.argv[1]
-    user = requests.get(f'https://jsonplaceholder.typicode.com/users/{userId}')
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
     todolist = requests.get(f'https://jsonplaceholder.typicode.com/todos')
-    name = user.json().get('name')
-    filename = userId + '.json'
+    name = user.json().get('username')
+    filename = "{}.json".format(sys.argv[1])
     tasks = []
     tasksD = {}
 
     for t in todolist.json():
-        if t.get('userId') == int(userId):
-            tasktodict = {"task": t.get('title'),
-                          "completed": t.get('completed'),
-                          "username": user.json().get('username')}
+        if t.get("userId") == int(userId):
+            tasktodict = {"task": t.get("title"),
+                          "completed": t.get("completed"),
+                          "username": user.json().get("username")}
             tasks.append(tasktodict)
     tasksD[userId] = tasks
-    with open(filename, mode='w') as myfile:
+    with open(filename, mode="w") as myfile:
         json.dump(tasksD, myfile)
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-''' Python script that all employees data information in json file '''
+"""
+Python script that all employees data information in json file
+"""
+
+import json
+import requests
+
 if __name__ == "__main__":
 
-    import requests
-    import sys
-    import json
-
-    user = requests.get(f'https://jsonplaceholder.typicode.com/users')
-    todolist = requests.get(f'https://jsonplaceholder.typicode.com/todos')
-    filename = 'todo_all_employees.json'
+    user = requests.get("https://jsonplaceholder.typicode.com/users")
+    todolist = requests.get("https://jsonplaceholder.typicode.com/todos")
+    filename = "todo_all_employees.json"
     tasks = []
     tasksD = {}
 
     for u in user.json():
         for t in todolist.json():
-            if t.get('userId') == u.get('id'):
-                tasktodict = {"task": t.get('title'),
-                              "completed": t.get('completed'),
-                              "username": u.get('username')}
+            if t.get("userId") == u.get("id"):
+                tasktodict = {"task": t.get("title"),
+                              "completed": t.get("completed"),
+                              "username": u.get("username")}
                 tasks.append(tasktodict)
-        tasksD[u.get('id')] = tasks
-    with open(filename, mode='w') as myfile:
+        tasksD[u.get("id")] = tasks
+    with open(filename, mode="w") as myfile:
         json.dump(tasksD, myfile)
